9f27f76bb5a8c0cac8fa68729bc6be5d625dbfba
Nigel Horne <njh@bandsman.co.uk>
2026-09-13 16:41:28 -0400

    Added 17,1

--- lib/Genealogy/Relationship/Name.pm
+++ lib/Genealogy/Relationship/Name.pm
@@ -163,6 +163,7 @@ Readonly::Hash my %EN_MALE_RELATIONSHIPS => (
 	'11,1' => 'eigth great-uncle',
 	'12,1' => 'ninth great-uncle',
 	'13,1' => 'tenth great-uncle',
+	'17,1' => 'fourteenth great-uncle',
 );
 
 Readonly::Hash my %EN_FEMALE_RELATIONSHIPS => (
@@ -290,6 +291,7 @@ Readonly::Hash my %EN_FEMALE_RELATIONSHIPS => (
 	'11,1' => 'eigth great-aunt',
 	'12,1' => 'ninth great-aunt',
 	'13,1' => 'tenth great-aunt',
+	'17,1' => 'fourteenth great-aunt',
 );
 
 # ---------------------------------------------------------------------------
